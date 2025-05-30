10fe7858f257becf9f84e16160e9c2d2784ccd38
Gyulo94 <qkrcjfrb112@gmail.com>
2025-05-30 18:48:47 +0900

    design: 404 페이지 추가

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,3 +1,24 @@
+"use client";
+
+import { useRouter } from "next/navigation";
+
 export default function NotFound() {
-  return <div>NotFound</div>;
+  const router = useRouter();
+  return (
+    <div className="text-center h-[60vh] flex flex-col justify-center">
+      <h2 className="text-primary text-3xl font-semibold">404 Not Found</h2>
+      <p className="text-gray-500 mt-4">
+        해당 경로에 맞는 페이지를 찾을 수 없습니다.
+      </p>
+      <div className="mt-8">
+        <button
+          type="button"
+          onClick={() => router.replace("/")}
+          className="bg-primary text-white rounded-xl px-4 py-2.5 hover:shadow-lg cursor-pointer"
+        >
+          메인으로 돌아가기
+        </button>
+      </div>
+    </div>
+  );
 }
