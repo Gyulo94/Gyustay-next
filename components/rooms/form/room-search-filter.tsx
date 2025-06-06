21868d3eb59e54d766017b331eda9a350610af13
Gyulo94 <qkrcjfrb112@gmail.com>
2025-06-06 23:56:16 +0900

    chore: debounce로 검색 성능 개선

--- components/rooms/form/room-search-filter.tsx
+++ components/rooms/form/room-search-filter.tsx
@@ -5,14 +5,30 @@ import { AiOutlineSearch } from "react-icons/ai";
 
 export default function RoomSearchFilter() {
   const { setQ } = useSearchStore();
+
+  const debounce = (func: Function, delay: number) => {
+    let timerId: NodeJS.Timeout;
+    return function (...args: any[]) {
+      clearTimeout(timerId);
+      timerId = setTimeout(() => {
+        func(...args);
+      }, delay);
+    };
+  };
+  function handleDebounceSearch(value: string) {
+    setQ(value);
+  }
+  const debouncedSearch = debounce(handleDebounceSearch, 1000);
+
+  const handleInputChange = (e: ChangeEvent<HTMLInputElement>) => {
+    debouncedSearch(e.target.value);
+  };
   return (
     <div className="grid sm:grid-cols-2 md:grid-cols-3 mb-10">
       <div className="flex items-center justify-center w-full gap-2">
         <Input
           type="search"
-          onChange={(e: ChangeEvent<HTMLInputElement>) => {
-            setQ(e.target.value);
-          }}
+          onChange={handleInputChange}
           placeholder="숙소명 검색"
         />
         <AiOutlineSearch className="size-6" />
