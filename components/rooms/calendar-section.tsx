68ab417f12ef9d8ccc846043ff684a00574cee5c
Gyulo94 <qkrcjfrb112@gmail.com>
2025-06-10 02:04:40 +0900

    design: 캘린더 모바일 css 크기 수정

--- components/rooms/calendar-section.tsx
+++ components/rooms/calendar-section.tsx
@@ -18,13 +18,10 @@ export default function CalendarSection() {
           ? `${filterValue.checkIn} ~ ${filterValue.checkOut}`
           : "체크인/체크아웃 날짜를 선택해주세요."}
       </div>
-      <div className="flex flex-col md:flex-row gap-4 justify-center">
+      <div className="flex flex-col md:flex-row md:gap-4 justify-center items-center">
         <Calendar
           mode="single"
           selected={new Date(filterValue.checkIn)}
-          // month={
-          //   filterValue.checkIn ? new Date(filterValue.checkIn) : new Date()
-          // }
           month={checkInMonth}
           onMonthChange={setCheckInMonth}
           onSelect={(date: Date | undefined) => {
@@ -35,15 +32,12 @@ export default function CalendarSection() {
             });
             setCheckInMonth(date);
           }}
-          className="h-full w-full"
+          className="w-5/6 h-[400px] md:size-full"
           hidden={{ before: new Date() }}
         />
         <Calendar
           mode="single"
           selected={new Date(filterValue.checkOut)}
-          // month={
-          //   filterValue.checkOut ? new Date(filterValue.checkOut) : new Date()
-          // }
           month={checkOutMonth}
           onMonthChange={setCheckOutMonth}
           onSelect={(date: Date | undefined) => {
@@ -54,7 +48,7 @@ export default function CalendarSection() {
             });
             setCheckOutMonth(date);
           }}
-          className="h-full w-full"
+          className="w-5/6 h-[400px] md:size-full"
           hidden={{
             before: filterValue.checkIn
               ? addDays(new Date(filterValue.checkIn), 1)
